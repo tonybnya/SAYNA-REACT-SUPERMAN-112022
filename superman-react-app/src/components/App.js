56455abe08fabbab907744452860b8722827325f
Tony <nya.tony2010@gmail.com>
2022-11-14 19:59:15 +0100

    Add: Bootstrap, Navbar component & routing

--- superman-react-app/src/components/App.js
+++ superman-react-app/src/components/App.js
@@ -1,14 +1,30 @@
 import '../styles/App.css';
-import Navbar from './Navbar';
-import Social from './Social';
-import Footer from './Footer';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Eshop from '../pages/Eshop';
+import MonCompte from '../pages/MonCompte';
+import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
+import NotFoundPage from '../pages/NotFoundPage';
+import Layout from '../pages/Layout';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Social />
-      <Footer />
+       <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='home' element={<Home />} />
+            <Route path='eshop' element={<Eshop />} />
+            <Route path='compte' element={<MonCompte />} />
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<SignUp />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter> 
     </>
   );
 }
--- superman-react-app/src/components/Navbar.js
+++ superman-react-app/src/components/Navbar.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import logo from '../assets/logos/logo_blanc.png';
+import '../styles/navbar.css';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="navbar navbar-expand-lg bg-dark">
+      <div className="container">
+        <NavLink to="/" className="navbar-brand">
+          <img width="10%" src={logo} alt="" />
+        </NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink to="home" className="nav-link" aria-current="page">HOME</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="eshop" className="nav-link">ESHOP</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="compte" className="nav-link">MON COMPTE</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
